e254b94dc012ac49813fdd428014910e59d65db4
Aaron Johnson <aj65461@gmail.com>
2021-05-16 17:37:33 -0400

    Add Home and About page

---
+++ src/pages/about.js
@@ -0,0 +1,13 @@
+import React from "react"
+import Layout from '../components/layout'
+
+const AboutPage = () => {
+   return (
+       <Layout pageTitle="About Me">
+           <p>I'm a painter from Jonesville, North Carolina.</p>
+       </Layout>
+   )
+   }
+
+export default AboutPage
+
