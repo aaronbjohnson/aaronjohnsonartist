2746af7cc13b55b8661a66ec442693aba7f5492e
Aaron Johnson <aj65461@gmail.com>
2021-05-20 08:37:53 -0400

    Add images locally to test new gallery

--- src/pages/gallery.js
+++ src/pages/gallery.js
@@ -1,12 +1,10 @@
 import React from "react"
 import Layout from '../components/layout'
-// import ImageGallery from '../components/image-gallery'
-import {Gallery} from "gatsby-theme-gallery"
 
 const GalleryPage = () => {
    return (
        <Layout pageTitle="Gallery">
-           <Gallery />
+           {/* <Gallery /> */}
        </Layout>
    )
    }
