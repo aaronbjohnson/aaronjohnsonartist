44143b153fcb842181328d42dc14b8c60acfdc49
Aaron Johnson <aj65461@gmail.com>
2021-05-24 18:16:11 -0400

    Add an ArtworkDetail component

---
+++ src/components/artwork-detail.js
@@ -0,0 +1,14 @@
+import React from 'react';
+
+const ArtworkDetail = () => {
+
+    return (
+        <div>
+            <h1>Name</h1>
+            <p>Info about the painting.</p>
+        </div>
+    )
+
+}
+
+export default ArtworkDetail;
--- src/pages/gallery.js
+++ src/pages/gallery.js
@@ -4,6 +4,7 @@ import Layout from '../components/layout'
 const GalleryPage = () => {
    return (
        <Layout pageTitle="Gallery">
+           <h3>Under Construction...</h3>
            {/* <Gallery /> */}
        </Layout>
    )
